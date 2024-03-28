928a017f6218639583900119d1960f35891943ff
yl5470 <yl5470@adcu.columbia.edu>
2024-03-28 19:59:40 -0400

    change setup

--- case3_g1_code.asm
+++ case3_g1_code.asm
@@ -1,18 +1,33 @@
-# include <P16F747.INC>
-    __CONFIG _CONFIG1, _FOSC_HS & _CP_OFF & _DEBUG_OFF & _VBOR_2_0 & _BOREN_0 
-    & _MCLR_ON & _PWRTE_ON & 
+#include <P16F747.INC>
 
-; Define your data section
-uDATA
-Temp res 1
+	__CONFIG _CONFIG1, _FOSC_HS & _CP_OFF & _DEBUG_OFF & _VBOR_2_0 & _BOREN_0 & _MCLR_ON & _PWRTE_ON & _WDT_OFF
+	__CONFIG _CONFIG2, _BORSEN_0 & _IESO_OFF &_FCMEN_OFF
 
-; Start of code section
-code
+State equ 22h		    ; Program state register
+Temp equ 23h		    ; Temporary register
+Timer1 equ 24h
+Timer2 equ 25h
+Timer3 equ 26h
+Timer equ 27h
+Octal equ 30h		    ; Octal switch register
+Mode equ 31h		    ; Mode register
+    
+    org 00h		    ; interrupt vector
+    goto SwitchCheck	    ; jump to interrupt service routine (dummy)
+    
+    org 04h		    ; interrupt vector
+    goto isrService	    ; jump to interrupt service routine (dummy)
+    
+    org 15h
 
+SwitchCheck
+    call initPort
+    
 initPort
     clrf PORTB               ; Clear PORTB
     clrf PORTC               ; Clear PORTC
-    bsf STATUS, RP0          ; Switch to Bank 1 for TRIS registers
+    
+    bcf STATUS, RP0          ; Switch to Bank 1 for TRIS registers
     clrf TRISB               ; Set all PORTB as output
     movlw 0xFF               ; Set all pins of PORTC as input (for button presses)
     movwf TRISC
@@ -21,18 +36,19 @@ initPort
     bcf STATUS, RP0          ; Switch back to Bank 0
     return
 
-    ; Wait for either the green or red button press
+setMode			; 
+    
 waitPress 
-    btfsc PORTC, 7           ; Check if green button is pressed (active low)
+    btfsc PORTC, 0           ; Check if green button is pressed (active low)
     goto GreenPress
-    btfsc PORTC, 6           ; Check if red button is pressed (active low)
+    btfsc PORTC, 1           ; Check if red button is pressed (active low)
     goto RedPress
     goto waitPress           ; No button press detected, loop back
 
     ; Handle green button press
 GreenPress
     call SwitchDelay          ; Debounce delay
-    btfss PORTC, 7           ; Check if green button is still pressed
+    btfss PORTC, 0           ; Check if green button is still pressed
     goto RedPress            ; If not, check red button
 
     ; Turn off the transistor (active high control assumed)
@@ -43,12 +59,12 @@ TransistorOff
     ; Handle red button press
 RedPress
     call SwitchDelay         ; Debounce delay
-    btfsc PORTC, 6           ; Check if red button is still pressed
+    btfsc PORTC, 1           ; Check if red button is still pressed
     goto RedRelease          ; If pressed, handle red button release
 
     ; Wait for red button release
 RedRelease
-    btfss PORTC, 6           ; Check if red button is released
+    btfss PORTC, 1           ; Check if red button is released
     goto RedRelease          ; Loop here until red button is released
     goto TransistorToggle    ; Proceed to toggle transistor
 
